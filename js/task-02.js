e8502d574d53d4ae203c4039bd1b2707e29fa077
AnitaPrzedwojewska <nisiek@wp.pl>
2023-12-25 00:28:07 +0100

    Added task 02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,23 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatoes",
+  "Herbs",
+  "Condiments",
 ];
+const listIngredients = document.querySelector("#ingredients");
+const listNodes = [];
+for (let i = 0; i < ingredients.length; i++) {
+  listNodes[i] = document.createElement("li");
+  listNodes[i].classList.add("item");
+  listNodes[i].textContent = ingredients[i];
+}
+// console.log(listNodes);
+// const listNodes = ingredients
+//   .map((ingredient) => `<li class="item">${ingredient}</li>`)
+//   .join("");
+// console.log(listNodes);
+// listNodes.forEach((node) => listIngredients.append(node));
+
+listIngredients.append(...listNodes);
