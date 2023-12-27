7e368d79f36b25acff608a75a88addfb6e396fc4
AnitaPrzedwojewska <nisiek@wp.pl>
2023-12-27 23:04:29 +0100

    Added task 04

---
+++ js/task-04.js
@@ -0,0 +1,15 @@
+const decrementButton = document.querySelector('[data-action="decrement"]');
+const incrementButton = document.querySelector('[data-action="increment"]');
+
+const valueElement = document.querySelector("#value");
+let counterValue = Number.parseInt(valueElement.textContent);
+
+decrementButton.addEventListener("click", () => {
+  counterValue--;
+  valueElement.textContent = counterValue;
+});
+
+incrementButton.addEventListener("click", () => {
+  counterValue++;
+  valueElement.textContent = counterValue;
+});
