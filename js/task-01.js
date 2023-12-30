3284593acb58ba09f07e0858a717ecb3ec82aa68
AnitaPrzedwojewska <nisiek@wp.pl>
2023-12-30 21:30:14 +0100

    Corrected task 01

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,6 @@
+const categoriesItems = document
+  .querySelector("#categories")
+  .querySelectorAll(".item");
 console.log(`Number of categories: ${categoriesItems.length}`);
 categoriesItems.forEach((item) =>
   console.log(
