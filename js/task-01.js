d4be889fe01cf3a5b405c01420e4d0ddd0a90a83
AnitaPrzedwojewska <nisiek@wp.pl>
2023-12-31 19:21:52 +0100

    Corrected task 01

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,21 @@
-const categoriesItems = document
-  .querySelector("#categories")
-  .querySelectorAll(".item");
-console.log(`Number of categories: ${categoriesItems.length}`);
-categoriesItems.forEach((item) =>
-  console.log(
-    `Category: ${item.querySelector("h2").textContent}\nElements: ${
-      item.querySelectorAll("li").length
-    }`
-  )
-);
+const categoriesItems = document.querySelector("#categories");
+console.log(`Number of categories: ${categoriesItems.children.length}`);
+
+let category = categoriesItems.firstElementChild;
+while (category !== null) {
+  console.log(`Category: ${category.children[0].textContent}\n
+  Elements: ${category.children[1].children.length}`);
+  category = category.nextElementSibling;
+}
+
+// const categoriesItems = document
+//   .querySelector("#categories")
+//   .querySelectorAll(".item");
+// console.log(`Number of categories: ${categoriesItems.length}`);
+// categoriesItems.forEach((item) =>
+//   console.log(
+//     `Category: ${item.querySelector("h2").textContent}\nElements: ${
+//       item.querySelectorAll("li").length
+//     }`
+//   )
+// );
